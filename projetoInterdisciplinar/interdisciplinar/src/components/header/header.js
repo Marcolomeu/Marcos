2d0d4927b23119f6e09af4f182581030d77442d1
Marcolomeu <marcoslopescassimiro@gmail.com>
2025-02-05 15:40:19 -0400

    finalização header

--- projetoInterdisciplinar/interdisciplinar/src/components/header/header.js
+++ projetoInterdisciplinar/interdisciplinar/src/components/header/header.js
@@ -1,33 +1,49 @@
-// src/components/header/header.js
 import React from 'react';
+import Image from "next/image";
 import styles from './header.module.css';
 
 const Header = () => {
-  return (
-    <header className={styles.header}>
-      <nav>
-        <ul className={styles.menu}>
-          <li><a href="#home">HOME</a></li>
-          <li><a href="#sobre">SOBRE</a></li>
-          <li>
-            <a href="#servicos">SERVIÇOS</a>
-            <ul className={styles.submenu}>
-              <li><a href="#performance">PERFORMANCE</a></li>
-              <li><a href="#remocao-virus">REMOÇÃO DE VÍRUS</a></li>
-              <li><a href="#manutencao">MANUTENÇÃO</a></li>
-              <li><a href="#seo">SEO</a></li>
-              <li><a href="#lojas-virtuais">LOJAS VIRTUAIS</a></li>
-              <li><a href="#criacao-sites">CRIAÇÃO DE SITES</a></li>
-            </ul>
-          </li>
-          <li><a href="#portfolio">PORTFÓLIO</a></li>
-          <li><a href="#glossario">GLOSSÁRIO</a></li>
-          <li><a href="#blog">BLOG</a></li>
-          <li><a href="#contato">CONTATO</a></li>
-        </ul>
-      </nav>
-      <p className={styles.contactNumber}></p>
-    </header>
+    return (
+        <header className={styles.header}>
+            <Image
+                src='/imagens/HeartLogo.png'
+                width={100}
+                height={100}
+                alt='html5'
+            />
+            <nav>
+                <ul className={styles.menu}>
+                    <li><a href="#home">HOME</a></li>
+                    <li>
+                        <a href="#médicos">MÉDICOS</a>
+                        <ul className={styles.submenu}>
+                            <li><a href="#Listar">Listar</a></li>
+                            <li><a href="#Adicionar">Adicionar</a></li>
+                            <li><a href="#Editar">Editar</a></li>
+                            <li><a href="#Excluir">Excluir</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href='#pacientes'>PACIENTES</a>
+                        <ul className= {styles.submenu}>
+                            <li><a href='#Listar'>Listar</a></li>
+                            <li><a href='#Adicionar'>Adicionar</a></li>
+                            <li><a href='#Editar'>Editar</a></li>
+                            <li><a href='#Excluir'>Excluir</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href='#agendamento'>AGENDAMENTO</a>
+                        <ul className={styles.submenu}>
+                            <li><a href='#Listar'>Listar Consultas</a></li>
+                            <li><a href='#Adicionar'>Agendar Consulta</a></li>
+                            <li><a href='#Editar'>Editar Agendamento</a></li>
+                            <li><a href='#Excluir'>Cancelar</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </nav>
+        </header>
   );
 }
 
