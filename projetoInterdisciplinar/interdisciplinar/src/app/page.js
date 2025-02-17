d612cbdc8b5f407fa379f28249e269e4595046eb
Marcolomeu <marcoslopescassimiro@gmail.com>
2025-02-17 15:23:27 -0400

    framer-motion

--- projetoInterdisciplinar/interdisciplinar/src/app/page.js
+++ projetoInterdisciplinar/interdisciplinar/src/app/page.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 import Header from '@/components/header/header.js';
 import styles from '../app/page.module.css';
 
 export default function Page() {
-
+    return (
+        <div className={styles.container}>
+            <Header />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 2 }}
+                className={styles.message}
+            >
+                <h1>Bem-vindo à Clínica!</h1>
+            </motion.div>
+        </div>
+    );
 }
-
-
-
